5528c5d719162539677c12a3b91bfd0f00634ceb
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-10 17:12:47 -0300

    Elevando a lista de times e adicionando estilos via props para o componente Time

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,18 @@
 import './Time.css'
 
 const Time = (props) => {
+
+  const estiloFundo = {
+    backgroundColor: props.secundaria
+  }
+
+  const estiloH3 = {
+    borderColor: props.primaria
+  }
+
   return (
-    <section className='time'>
-      <h3>{props.nome}</h3>
+    <section className='time' style={estiloFundo}>
+      <h3 style={estiloH3}>{props.nome}</h3>
     </section>
   )
 }
